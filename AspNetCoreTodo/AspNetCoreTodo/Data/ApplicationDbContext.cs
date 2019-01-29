c6eb73648e4760c8afb086198b36a0282b345f62
Anila <anila.ravindra.lm@liquidmodules.com>
2019-01-29 16:14:53 +0000

    aspnetcoretodo
    aspnetcoretodo

---
+++ AspNetCoreTodo/AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser,IdentityRole,string>
+    {
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+         
+        public DbSet<TodoItem> Items { get; set; }
+    }
+}
